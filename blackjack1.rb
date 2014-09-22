2e344aec680f4530cdfa5e7ee8320f8b2d6451bf
Dawn Miceli <dawnmiceli100@yahoo.com>
2014-09-22 12:51:23 -0400

    Make adjustments after watching course videos

--- blackjack1.rb
+++ blackjack1.rb
@@ -12,13 +12,11 @@
 # If the dealer goes over 21 with a hit, she loses.
 # If the dealer stays, then the player total and the dealer total are compared and the highest total wins.
 
- 
 SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 deck = SUITS.product(CARDS)
 player_cards = []
 dealer_cards = []
-game_over = false
 
 puts "Welcome to Blackjack!" 
 
@@ -33,19 +31,21 @@ def calculate_total(cards)
   cards.each do |card|
     if face_cards.include?(card[1]) 
       total += 10
-    elsif card[1] == "Ace"
-      if total < 11
-        total += 11
-      else
-        total += 1
-      end
+    elsif card[1] == "Ace" # Use value of 11 for now. Change to 1 later if total > 21
+      total += 11
     else
       total += card[1].to_i
     end        
-  end 
-  return total       
+  end
+
+  # Use value of 1 instead of 11 for Aces if the total > 21
+  cards.select{ |card| card[1] == "Ace"}.count.times do
+    total -= 10 if total > 21
+  end    
+  total       
 end  
 
+# Deal 2 cards to player and 2 cards to dealer
 deck = new_deck
 puts "I will deal two cards to each of us..."
 puts "" 
@@ -59,54 +59,65 @@ player_cards.each { |card| puts "-> #{card[1]} of #{card[0]}" }
 player_total = calculate_total(player_cards)
 puts "-> Total: #{player_total}"
 puts ""
+
 puts "Dealer cards: "
 dealer_cards.each { |card| puts "-> #{card[1]} of #{card[0]}" } 
 dealer_total = calculate_total(dealer_cards)
 puts "-> Total: #{dealer_total}" 
 puts ""
 
-puts "What would you like to do? Enter 1 for 'hit' and 2 for 'stay'"
-hit_or_stay = gets.chomp
-while (hit_or_stay != "2") && (!game_over)
+# Player's turn
+if player_total == 21
+  puts "Congratulations! You have hit blackjack!"
+  exit
+end
+
+while player_total < 21
+  puts "What would you like to do? Enter 'H' for hit and 'S' for stay."
+  hit_or_stay = gets.chomp.downcase
+  if !['h', 's'].include?(hit_or_stay)
+    puts "You did not enter a valid response. Enter 'H' for hit and 'S' for stay. "
+    next
+  end  
+  if hit_or_stay == "s"
+    puts "You have decided to stay."
+    break
+  end
+  
+  # chose hit  
   player_cards << deck.pop
   puts "I just dealt you a #{player_cards.last[1]} of #{player_cards.last[0]}"
   player_total = calculate_total(player_cards)
   puts "Your total is now: #{player_total}"
+
   if player_total == 21
     puts "Congratulations! You have hit blackjack!"
-    game_over = true
+    exit
   elsif player_total > 21
     puts "You have busted!"
-    game_over = true
-  else      
-    puts "What would you like to do? Enter 1 for 'hit' and 2 for 'stay'"
-    hit_or_stay = gets.chomp
+    exit
   end  
 end 
 
-if !game_over
-  puts "It is now my turn."
-  dealer_total = calculate_total(dealer_cards)
-  if dealer_total > player_total
-    puts "I have won! My total is higher than your total"
+# Dealer's turn
+puts "It is now my turn."
+
+dealer_total = calculate_total(dealer_cards)
+if (dealer_total > player_total) && (dealer_total > 17)
+  puts "I have won! My total is higher than your total"
+else
+  while ((dealer_total < 21) && (dealer_total < player_total)) || dealer_total < 17
+    dealer_cards << deck.pop 
+    puts "I just dealt myself a #{dealer_cards.last[1]} of #{dealer_cards.last[0]}"
+    dealer_total =  calculate_total(dealer_cards)
+    puts "My total is now: #{dealer_total}" 
+  end
+
+  if dealer_total == 21
+    puts "I have hit blackjack! Dealer has won!"
+  elsif dealer_total > 21
+    puts "I have busted! You have won!" 
   else
-    while (dealer_total < 21) && (dealer_total < player_total)
-      dealer_cards << deck.pop 
-      puts "I just dealt myself a #{dealer_cards.last[1]} of #{dealer_cards.last[0]}"
-      dealer_total =  calculate_total(dealer_cards)
-      puts "My total is now: #{dealer_total}" 
-    end
-
-    if dealer_total == 21
-      puts "I have hit blackjack! Dealer has won!"
-    elsif dealer_total > 21
-      puts "I have busted! You have won!" 
-    else
-      puts "I have won! My total is higher than your total."
-    end  
+    puts "I have won! My total is higher than your total."
   end  
-end
-  
-
-
-
+end  
